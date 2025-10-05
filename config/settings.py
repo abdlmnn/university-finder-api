dbfc10f61e4510fe48fd70ef1f1a036bcff9bb30
Abdulmanan <mohammadabdulmanan24@gmail.com>
2025-10-06 04:13:51 +0800

    New database postgreSQL

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
+        'PORT': config('DB_PORT', default='5433'),
     }
 }
 
