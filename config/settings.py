770b3016c6eb285b293859e3d1dcb8727a69df60
Abdulmanan <mohammadabdulmanan24@gmail.com>
2025-10-06 15:08:59 +0800

    New auth id

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ from decouple import config
 
 import dj_database_url
 
-
+#Hi
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
